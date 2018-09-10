f51fc02de6d77b6dbc5f862ae0afe1700a7a5a4b
Michael J. Calkins <michaeljamescalkins@gmail.com>
2018-09-10 07:39:48 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,41 +1,86 @@
 // @flow
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
+import chunk from "lodash/chunk";
 
 type Props = {};
 
 type State = {
-  value: string
+  frames: string[],
+  currentFrame: number
 };
 
 class App extends Component<Props, State> {
   state = {
-    value: ""
+    frames: [],
+    currentFrame: 0
   };
 
-  handleGetIpsum = () => {
-    axios.get("https://baconipsum.com/api/?type=meat-and-filler").then(response => {
-      this.setState({
-        value: response.data
-      });
+  componentDidMount = () => {
+    this.handleFetchMovie();
+  };
+
+  /**
+    Loads the ASCII movie from given text file.
+    Using an encoded format, described on
+    http://www.asciimation.co.nz/asciimation/ascii_faq.html
+    In short:
+        67x14 chars
+        lines separated with \n
+        first line is a number telling delay in number of frames
+        13 lines effective frame size
+        15 frames per second
+  */
+  handleFetchMovie = () => {
+    axios.get("/movies/sw1.txt").then(response => {
+      this.setState(
+        {
+          frames: chunk(response.data.split("\n"), 14)
+        },
+        () => {
+          console.log(this.state.frames[this.state.currentFrame]);
+
+          this.playFrame();
+        }
+      );
     });
+  };
+
+  playFrame = () => {
+    setTimeout(() => {
+      if (this.state.currentFrame - 1 > this.state.frames.length) {
+        this.setState(
+          {
+            currentFrame: 0
+          },
+          () => {
+            this.playFrame();
+          }
+        );
+        return;
+      }
+
+      this.setState(
+        {
+          currentFrame: this.state.currentFrame + 1
+        },
+        () => {
+          this.playFrame();
+        }
+      );
+    }, 1000 / 5);
+  };
+
+  renderCurrentFrame = () => {
+    if (!this.state.frames[this.state.currentFrame]) return "";
+    return this.state.frames[this.state.currentFrame].join("\n");
   };
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Bacon Ipsum Generator</h1>
-        </header>
-        <p className="App-intro">
-          <button onClick={this.handleGetIpsum}>Generate Ipsum</button>
-        </p>
-        <p>
-          <textarea className="App-textarea" value={this.state.value} />
-        </p>
+        <pre className="App-player">{this.renderCurrentFrame()}</pre>
       </div>
     );
   }

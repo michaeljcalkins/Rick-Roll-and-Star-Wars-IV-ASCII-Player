a285e552ed24a6c7b847c0fcd028cc0a56b0a301
Michael J. Calkins <michaeljamescalkins@gmail.com>
2018-09-10 21:57:42 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -4,84 +4,107 @@ import "./App.css";
 import axios from "axios";
 import chunk from "lodash/chunk";
 
+/**
+  Loads the ASCII movie from given text file.
+  Using an encoded format, described on
+  http://www.asciimation.co.nz/asciimation/ascii_faq.html
+  In short:
+      67x14 chars
+      lines separated with \n
+      first line is a number telling delay in number of frames
+      13 lines effective frame size
+      15 frames per second
+*/
+
 type Props = {};
 
 type State = {
   frames: string[],
-  currentFrame: number
+  currentFrame: number,
+  currentMovie: "short_intro" | "rick_roll" | "sw1"
 };
 
 class App extends Component<Props, State> {
   state = {
     frames: [],
-    currentFrame: 0
+    currentFrame: 0,
+    currentMovie: "short_intro"
   };
+
+  frameTimeout = null;
 
   componentDidMount = () => {
     this.handleFetchMovie();
   };
 
-  /**
-    Loads the ASCII movie from given text file.
-    Using an encoded format, described on
-    http://www.asciimation.co.nz/asciimation/ascii_faq.html
-    In short:
-        67x14 chars
-        lines separated with \n
-        first line is a number telling delay in number of frames
-        13 lines effective frame size
-        15 frames per second
-  */
   handleFetchMovie = () => {
-    axios.get("/movies/sw1.txt").then(response => {
+    axios.get(`/movies/${this.state.currentMovie}.txt`).then(response => {
       this.setState(
         {
           frames: chunk(response.data.split("\n"), 14)
         },
-        () => {
-          console.log(this.state.frames[this.state.currentFrame]);
-
-          this.playFrame();
-        }
+        () => this.playFrame()
       );
     });
   };
 
   playFrame = () => {
-    setTimeout(() => {
-      if (this.state.currentFrame - 1 > this.state.frames.length) {
-        this.setState(
-          {
-            currentFrame: 0
-          },
-          () => {
-            this.playFrame();
-          }
-        );
-        return;
-      }
+    if (!this.state.frames[this.state.currentFrame]) {
+      this.setState(
+        {
+          currentFrame: 0
+        },
+        () => this.playFrame()
+      );
+      return;
+    }
 
+    const frameDelay = (1000 / 15) * parseInt(this.state.frames[this.state.currentFrame][0], 10);
+    this.frameTimeout = setTimeout(() => {
       this.setState(
         {
           currentFrame: this.state.currentFrame + 1
         },
-        () => {
-          this.playFrame();
-        }
+        () => this.playFrame()
       );
-    }, 1000 / 5);
+    }, frameDelay);
   };
 
   renderCurrentFrame = () => {
     if (!this.state.frames[this.state.currentFrame]) return "";
-    return this.state.frames[this.state.currentFrame].join("\n");
+    const frameLines = this.state.frames[this.state.currentFrame];
+    return frameLines.slice(1, frameLines.length).join("\n");
+  };
+
+  stopMovie = () => {
+    clearTimeout(this.frameTimeout);
+  };
+
+  handleSetMovie = (newMovie: "short_intro" | "sw1" | "rick_roll") => {
+    this.stopMovie();
+    this.setState(
+      {
+        currentMovie: newMovie,
+        currentFrame: 0
+      },
+      this.handleFetchMovie()
+    );
   };
 
   render() {
     return (
-      <div className="App">
-        <pre className="App-player">{this.renderCurrentFrame()}</pre>
-      </div>
+      <React.Fragment>
+        <div className="App-player">{this.renderCurrentFrame()}</div>
+        <div className="App-navigation">
+          <button className="btn-flat waves-effect waves-light" onClick={() => this.handleSetMovie("rick_roll")}>
+            Rick Roll
+          </button>
+          &nbsp;
+          <button className="btn-flat waves-effect waves-light" onClick={() => this.handleSetMovie("sw1")}>
+            Star Wars IV
+          </button>
+        </div>
+      </React.Fragment>
     );
   }
 }
